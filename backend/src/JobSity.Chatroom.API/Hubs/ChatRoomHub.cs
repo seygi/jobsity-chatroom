41fc9bad6ac76feee672310231ea9de5a1ca97c5
seygi.kutani <seygi@outlook.com>
2023-03-03 19:18:30 -0300

    - fix namespace

--- backend/src/JobSity.Chatroom.API/Hubs/ChatRoomHub.cs
+++ backend/src/JobSity.Chatroom.API/Hubs/ChatRoomHub.cs
@@ -1,4 +1,5 @@
 ﻿using JobSity.Chatroom.Application.Features.ChatroomMessages.CreateMessage.UseCase;
+using JobSity.Chatroom.Application.Features.Chatrooms.CreateRoom.UseCase;
 using JobSity.Chatroom.Application.Shared.UseCase;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
