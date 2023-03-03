febbdefe3f5de5f8ee8de001823323f0498b9d4a
seygi.kutani <seygi@outlook.com>
2023-03-03 17:50:38 -0300

    - login route on api

--- backend/src/JobSity.Chatroom.API/Controllers/UserController.cs
+++ backend/src/JobSity.Chatroom.API/Controllers/UserController.cs
@@ -1,14 +1,10 @@
 ﻿using JobSity.Chatroom.API.Transport.V1.CreateUser;
 using JobSity.Chatroom.Application.Features.ChatroomMessages.CreateMessage.UseCase;
 using JobSity.Chatroom.Application.Features.Users.CreateUser.UseCase;
+using JobSity.Chatroom.Application.Features.Users.LoginUser.UseCase;
 using JobSity.Chatroom.Application.Shared.UseCase;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using NetDevPack.Identity.Jwt;
 using NetDevPack.Identity.Model;
-using NetDevPack.Identity.User;
 
 namespace JobSity.Chatroom.API.Controllers
 {
@@ -16,12 +12,10 @@ namespace JobSity.Chatroom.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        //private readonly SignInManager<IdentityUser> _signInManager;
-        //private readonly UserManager<IdentityUser> _userManager;
-        //private readonly AppJwtSettings _appJwtSettings;
-        //private readonly IAspNetUser _user;
-
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult> CreateUser([FromBody] RegisterUser newUser,
             [FromServices]IUseCase<CreateUserInput, CreateUserOutput> useCase,
             CancellationToken cancellationToken)
@@ -34,35 +28,23 @@ namespace JobSity.Chatroom.API.Controllers
             return Problem();
         }
 
-        //[HttpPost]
-        //[Route("login")]
-        //public async Task<IActionResult> Login(LoginUser loginUser)
-        //{
-        //    if (!ModelState.IsValid) return CustomResponse(ModelState);
+        [HttpPost]
+        [Route("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Login([FromBody] LoginUser loginUser,
+            [FromServices] IUseCase<LoginUserInput, LoginUserOutput> useCase,
+            CancellationToken cancellationToken)
+        {
+            var result = await useCase.ExecuteAsync(LoginUserInput.Create(loginUser.Email, loginUser.Password),cancellationToken);
 
-        //    var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+            if (result.Success)
+                return Ok(LoginUserResponse.Create(result));
 
-        //    if (result.Succeeded)
-        //    {
-        //        var fullJwt = GetFullJwt(loginUser.Email);
-        //        return CustomResponse(fullJwt);
-        //    }
-
-        //    if (result.IsLockedOut)
-        //    {
-        //        AddError("This user is temporarily blocked");
-        //        return CustomResponse();
-        //    }
-
-        //    AddError("Incorrect user or password");
-        //    return CustomResponse();
-        //}
-
-        //[Authorize]
-        //[HttpGet("get-user-id")]
-        //public async Task<IActionResult> GetUserId()
-        //{
-        //    return CustomResponse(_user.GetUserId().ToString());
-        //}
+            return Problem();
+        }
     }
 }
---
+++ backend/src/JobSity.Chatroom.API/Transport/V1/LoginUser/LoginUserResponse.cs
@@ -0,0 +1,15 @@
+﻿using JobSity.Chatroom.Application.Features.Users.LoginUser.UseCase;
+
+namespace JobSity.Chatroom.API.Transport.V1.CreateUser
+{
+    public sealed class LoginUserResponse
+    {
+        public string UserJwt { get; set; }
+        public LoginUserResponse(string userJwt)
+        {
+            UserJwt = userJwt;
+        }
+        public static LoginUserResponse Create(LoginUserOutput outputUseCase)
+            => new(outputUseCase.UserJwt);
+    }
+}
