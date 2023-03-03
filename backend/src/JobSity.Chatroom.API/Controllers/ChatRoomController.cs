1756dd1fd08c8d5105148392c91d2ff96d1a64f1
seygi.kutani <seygi@outlook.com>
2023-03-03 19:19:06 -0300

    - route to create room

---
+++ backend/src/JobSity.Chatroom.API/Controllers/ChatRoomController.cs
@@ -0,0 +1,24 @@
+﻿using JobSity.Chatroom.API.Transport.V1.CreateRoom;
+using JobSity.Chatroom.Application.Features.Chatrooms.CreateRoom.UseCase;
+using JobSity.Chatroom.Application.Shared.UseCase;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSity.Chatroom.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public class ChatRoomController : ControllerBase
+    {
+
+        [HttpPost()]
+        public async Task<IActionResult> CreateRoom([FromBody] CreateRoomRequest request,
+            [FromServices] IUseCase<CreateRoomInput, CreateRoomOutput> useCase,
+            CancellationToken cancellationToken)
+        {
+            await useCase.ExecuteAsync(CreateRoomInput.Create(request.Name), cancellationToken);
+            return Ok();
+        }
+    }
+}
---
+++ backend/src/JobSity.Chatroom.API/Transport/V1/CreateRoom/CreateRoomRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace JobSity.Chatroom.API.Transport.V1.CreateRoom
+{
+    public class CreateRoomRequest
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
