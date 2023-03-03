06a5f82b9b0cb27a4d3e57d1ce0e7da7b2fcd2e1
seygi.kutani <seygi@outlook.com>
2023-03-03 15:39:45 -0300

    - automapper

---
+++ backend/src/JobSity.Chatroom.Application/Shared/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using JobSity.Chatroom.Application.Shared.Chat;
+using JobSity.Chatroom.Application.Shared.Chat.ViewModels;
+using JobSityNETChallenge.Application.ViewModels;
+
+namespace JobSity.Chatroom.Application.Shared.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<ChatRoom, ChatRoomViewModel>();
+            CreateMap<ChatMessage, ChatMessageViewModel>()
+                .ConstructUsing(e => new ChatMessageViewModel { CreatedUserName = e.CreatedUserName, CreatedUserId = e.CreatedUserId, Text = e.Text, CreatedOn = e.CreatedOn });
+        }
+    }
+}
---
+++ backend/src/JobSity.Chatroom.Application/Shared/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace JobSity.Chatroom.Application.Shared.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            //CreateMap<ChatRoomViewModel, CreateChatRoomCommand>()
+            //    .ConstructUsing(c => new CreateChatRoomCommand(c.Name, c.CreatedByUserId));
+
+            //CreateMap<ChatRoomViewModel, UpdateChatRoomCommand>()
+            //    .ConstructUsing(c => new UpdateChatRoomCommand(c.Id, c.Name));
+
+            //CreateMap<ChatMessageViewModel, CreateChatMessageCommand>()
+            //    .ConstructUsing(c => new CreateChatMessageCommand(c.CreatedByUserId, c.ChatRoomId, c.CreatedByUserName, c.Text));
+        }
+    }
+}
