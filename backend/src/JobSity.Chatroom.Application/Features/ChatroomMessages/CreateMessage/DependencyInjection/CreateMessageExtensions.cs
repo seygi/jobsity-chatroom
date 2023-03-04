b4ce0eaf6d2f85253cc83d0e6f8027604f81749c
seygi.kutani <seygi@outlook.com>
2023-03-04 11:44:35 -0300

    - remove unused using

--- backend/src/JobSity.Chatroom.Application/Features/ChatroomMessages/CreateMessage/DependencyInjection/CreateMessageExtensions.cs
+++ backend/src/JobSity.Chatroom.Application/Features/ChatroomMessages/CreateMessage/DependencyInjection/CreateMessageExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using FluentValidation;
 using JobSity.Chatroom.Application.Features.ChatroomMessages.CreateMessage.UseCase;
 using JobSity.Chatroom.Application.Features.ChatroomMessages.CreateMessage.Validators;
-using JobSity.Chatroom.Application.Features.Chatrooms.CreateRoom.UseCase;
-using JobSity.Chatroom.Application.Features.Chatrooms.CreateRoom.Validators;
 using JobSity.Chatroom.Application.Shared.UseCase;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
