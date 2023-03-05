1649f514a7c80a71c6eb3f59d8267ed1a37b8995
seygi.kutani <seygi@outlook.com>
2023-03-04 23:57:15 -0300

    - initial test project

---
+++ backend/test/JobSity.Chatroom.Tests.Unit/WebApi/Transport/V1/CreateRoom/CreateRoomRequestTest.cs
@@ -0,0 +1,28 @@
+﻿using JobSity.Chatroom.API.Transport.V1.CreateRoom;
+using System.Text.Json.Serialization;
+
+namespace JobSity.Chatroom.Tests.Unit.WebApi.Transport.V1.CreateRoom
+{
+    public class CreateRoomRequestTest
+    {
+        [Theory(DisplayName = "Should Create Object")]
+        [InlineData(default)]
+        [InlineData("A")]
+        [InlineData("Z")]
+        public void ShouldCreateObject(string name)
+        {
+            // arrange - act
+            var result = new CreateRoomRequest(name);
+            result.Name.Should().Be(name);
+
+
+            // assert
+            AssertProperties(result, name);
+        }
+
+        private static void AssertProperties(CreateRoomRequest result, string name)
+        {
+            result.Name.ShouldBe().BeEquivalentTo(name);
+        }
+    }
+}
