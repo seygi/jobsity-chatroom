68cb2445859843ae984c03b1c576e18b906d807d
seygi.kutani <seygi@outlook.com>
2023-03-06 06:25:38 -0300

    - removed unused items

--- execute.bat
+++
@@ -1,9 +0,0 @@
-docker-compose up -d
-@echo off
-cd backend/src/JobSity.Chatroom.Consumer.BotStockSearch
-dotnet build
-echo "Waiting for 10 seconds..."
-timeout /t 10
-echo "Done!"
-dotnet run
-pause
